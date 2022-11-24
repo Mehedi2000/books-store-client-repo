767f1ea9ace47664c681a5305a241c492acef8cd
Mehedi Hasan <mh4054528@gmail.com>
2022-11-24 23:55:16 +0600

    added category in homepage

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="mt-8">
+      <h1 className="text-4xl font-bold text-center">All Categories </h1>
+      <div className="bg-base-200 flex w-4/5 mx-auto justify-evenly mt-8 p-40">
+        {categories.map((category) => (
+          <p key={category._id}>
+            <Link to={`/category/${category.categoryId}`}>
+              <button className="btn btn-outline btn-primary">
+                {category.title}
+              </button>
+            </Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
+import Categories from "../Categories/Categories";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Categories></Categories>
       <AboutUs></AboutUs>
     </div>
   );
