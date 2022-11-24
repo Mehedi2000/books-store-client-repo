17b8b4ae03ec41543bd7ce74c060774a63896b07
Mehedi Hasan <mh4054528@gmail.com>
2022-11-24 16:13:38 +0600

    added error page

---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <div>
+        <img
+          className="w-3/5 mx-auto mt-28"
+          src="https://static.vecteezy.com/system/resources/previews/002/852/982/non_2x/modern-colorful-404-page-not-found-error-background-illustration-404-error-background-can-use-for-web-banner-infographics-vector.jpg"
+          alt=""
+        />
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
